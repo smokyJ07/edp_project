ccd149a02da44c2d0366bb6cc41baa35bae88814
Jonas <jonas.rauchhaus@gmail.com>
2019-03-17 22:25:13 +0000

    finished menu.sh

--- menu.sh
+++ menu.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
+cd /
+cd /home/pi/Desktop/edp_program
 choice="1"
 until [ "$choice" = "0" ]; do
 	echo -n "Enter a program to execute (1-SR,2-APP,0-quit):"
-	read choice
+	read -t10 choice
+	if [ $? -gt 128 ]; then
+		echo "Timed out, SR starts on default..."
+		choice="1"
+	fi
 	case $choice  in
 		1 ) python3 p_recog4.py;;
 		2 ) python3 app_recog.py;;
@@ -10,3 +16,4 @@ until [ "$choice" = "0" ]; do
 		* ) echo "Enter 1,2 or 0";;
 	esac
 done
+cd /
